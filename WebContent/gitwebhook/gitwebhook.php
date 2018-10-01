334a552fc96f34c4f51742ea87aeb23649e504ac
Stefano Balietti <futur.dorko@gmail.com>
2018-10-01 14:56:56 -0400

    webhook

--- WebContent/gitwebhook/gitwebhook.php
+++ WebContent/gitwebhook/gitwebhook.php
@@ -2,8 +2,7 @@
 
 require(__DIR__ . '/secret.php');
 
-function validateSignature($payload) {
-  $gitHubSignatureHeader = @$_SERVER['HTTP_X_HUB_SIGNATURE'];
+function validateSignature($gitHubSignatureHeader, $payload) {
   list ($algo, $gitHubSignature) = explode("=", $gitHubSignatureHeader);
   if ($algo !== 'sha1') {
     // See https://developer.github.com/webhooks/securing/
@@ -41,8 +40,9 @@ if (strcasecmp($contentType, 'application/json') != 0) {
 // Receive the RAW post data.
 $content = trim(file_get_contents("php://input"));
 
-if (!validateSignature($content)) {
-  logIt("Invalid signature: " . @$_SERVER['HTTP_X_HUB_SIGNATURE']);
+$gitHubSignatureHeader = @$_SERVER['HTTP_X_HUB_SIGNATURE'];
+if (!validateSignature($gitHubSignatureHeader, $content)) {
+  logIt("Invalid signature: " . $gitHubSignatureHeader);
 }
  
 // Attempt to decode the incoming RAW post data from JSON.
@@ -61,32 +61,27 @@ $updated = array();
 // There might be more than 1 commit from last push.
 $commits = $decoded->commits;
 
+foreach ($commits as $commit_data) {
+    $mod_files = $commit_data->modified;
+    foreach ($mod_files as $f) {
+      // Ignore changes to gitwebhook directory (must update manually).
+      if (strpos($f, 'gitwebhook') !== false) continue;
+      // Ignore changes outside WebContent.
+      if (strpos($f, 'WebContent/') !== 0) continue;
+      // Ignore files already copied.
+      if ($updated[$f]) continue;
+      // Copy file.
+      // file_put_contents("./file_" . count($updated), fopen($prefix . $f), 'r'));
+      // Mark updated.
+      $updated[$f] = TRUE;
+   }
+}     
+
 // Save last payload.
-// $my_file = 'lastPayload.txt';
-// $handle = fopen($my_file, 'w') or die('Cannot open file:  '.$my_file);
-// fwrite($handle, $content);
-// fclose($handle);
-// logIt("COMMITS: " . count($commits), TRUE);
-
-
-// foreach ($commits as $mod_files) {
-//     foreach ($mod_files as $f) {
-//       // Ignore changes to gitwebhook directory (must update manually).
-//       if (strpos($f, 'gitwebhook') !== false) continue;
-//       // Ignore changes outside WebContent.
-//       if (strpos($f, 'WebContent/") !== 0) continue;
-//       if ($updated[$f]) continue;
-//       // Copy file.
-//       file_put_contents("./file", fopen($prefix . $f), 'r'));
-//       // Mark updated.
-//       $updated[$f] = TRUE;
-// }
-// 
-// // Save last payload.
-// $my_file = 'lastPayload.txt';
-// $handle = fopen($my_file, 'w') or die('Cannot open file:  '.$my_file);
-// fwrite($handle, $content);
-// fclose($handle);
-// logIt("OK. Files updated: " . count($updated), TRUE);
+$my_file = 'lastPayload.txt';
+$handle = fopen($my_file, 'w') or die('Cannot open file:  '.$my_file);
+fwrite($handle, var_export($decoded, true));
+fclose($handle);
+logIt("OK. Files updated: " . count($updated), TRUE);
 
 
