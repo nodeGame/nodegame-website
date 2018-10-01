756d2126e4f21b5e245d6e0a34f1c1dbfbfdee2e
Stefano Balietti <futur.dorko@gmail.com>
2018-10-01 11:45:04 -0400

    webhook

---
+++ WebContent/gitwebhook.php
@@ -0,0 +1,27 @@
+<?php
+
+require('secret.php');
+
+// Make sure that it is a POST request.
+if (strcasecmp($_SERVER['REQUEST_METHOD'], 'POST') != 0) {
+    throw new Exception('Request method must be POST!');
+}
+ 
+// Make sure that the content type of the POST request has been set to application/json
+$contentType = isset($_SERVER["CONTENT_TYPE"]) ? trim($_SERVER["CONTENT_TYPE"]) : '';
+if (strcasecmp($contentType, 'application/json') != 0) {
+    throw new Exception('Content type must be: application/json');
+}
+ 
+// Receive the RAW post data.
+$content = trim(file_get_contents("php://input"));
+ 
+// Attempt to decode the incoming RAW post data from JSON.
+$decoded = json_decode($content, true);
+ 
+// If json_decode failed, the JSON is invalid.
+if (!is_array($decoded)) {
+    throw new Exception('Received content contained invalid JSON!');
+}
+ 
+//Process the JSON.
--- WebContent/nodegame-installer.js
+++
@@ -1,1 +0,0 @@
-/home/stefano/www/nodegame/bin/nodegame-installer.js
