095b9d497044821c137114a3652af722b3f3e031
Stefano Balietti <futur.dorko@gmail.com>
2018-10-01 12:40:53 -0400

    webhook

--- WebContent/gitwebhook/gitwebhook.php
+++ WebContent/gitwebhook/gitwebhook.php
@@ -1,10 +1,22 @@
 <?php
 
-require('secret.php');
+require(__DIR__ . '/secret.php');
+
+function validateSignature($gitHubSignatureHeader, $payload) {
+  list ($algo, $gitHubSignature) = explode("=", $gitHubSignatureHeader);
+  if ($algo !== 'sha1') {
+    // See https://developer.github.com/webhooks/securing/
+    return false;
+  }
+  $payloadHash = hash_hmac($algo, $payload, $secret);
+  return hash_equals($payloadHash, $gitHubSignature);
+}
+
+$str = "OK";
 
 // Make sure that it is a POST request.
 if (strcasecmp($_SERVER['REQUEST_METHOD'], 'POST') != 0) {
-    throw new Exception('Request method must be POST!');
+    $str = 'Request method must be POST!';
 }
  
 // Make sure that the content type of the POST
@@ -13,7 +25,7 @@ $contentType = isset($_SERVER["CONTENT_TYPE"]) ?
                  trim($_SERVER["CONTENT_TYPE"]) : '';
                  
 if (strcasecmp($contentType, 'application/json') != 0) {
-    throw new Exception('Content type must be: application/json');
+    $str = 'Content type must be: application/json';
 }
  
 // Receive the RAW post data.
@@ -24,11 +36,23 @@ $decoded = json_decode($content, true);
  
 // If json_decode failed, the JSON is invalid.
 if (!is_array($decoded)) {
-    throw new Exception('Received content contained invalid JSON!');
+    $str = 'Received content contained invalid JSON!';
 }
- 
-// Process the JSON.
 
-$my_file = 'file.txt';
-$handle = fopen($my_file, 'w') or die('Cannot open file:  '.$my_file);
-fwrite($handle, $decoded);
+if (!validateSignature($decoded)) {
+  $str = "Invalid signature";
+}
+
+// Process the JSON.
+$mydate = date('l jS \of F Y h:i:s A'); 
+$my_file = 'log.txt';
+$handle = fopen($my_file, 'a') or die('Cannot open file:  '.$my_file);
+fwrite($handle, $mydate . " > " . $str . "\n");
+fclose($handle);
+
+if ($str == "OK" ) {
+  $my_file = 'file.txt';
+  $handle = fopen($my_file, 'w') or die('Cannot open file:  '.$my_file);
+  fwrite($handle, $content);
+  fclose($handle);
+}
